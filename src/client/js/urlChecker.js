4c4efe453cde9a004f91dc24b3029671ad0a7a2f
Reem Alharbi <reem@Reems-MacBook-Air.local>
2021-02-16 21:53:35 +0300

    first commit

---
+++ src/client/js/urlChecker.js
@@ -0,0 +1,11 @@
+function checkForURL(inputURL) {
+    var regex = inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+
+     if(regex == null){
+         return alert("no");
+     } else{
+     return alert("yes");
+     }
+ }
+
+ export { checkForURL }
